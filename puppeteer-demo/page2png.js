66a03ecebf0c52be9db883fb847b8a8c8085f993
Go7hic <gtfx0209@qq.com>
2019-09-16 22:01:18 +0800

    update

--- puppeteer-demo/page2png.js
+++ puppeteer-demo/page2png.js
@@ -1,10 +1,16 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
+  const tmpl = fs.readFileSync('./tmp/tmpl.html', 'utf-8')
+  const content = tmpl;
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://baidu.com');
-  await page.screenshot({path: 'baidu.png'});
-
+  const override = Object.assign(page.viewport(), {width: await page.evaluate(() => document.body.clientWidth), height: await page.evaluate(() => document.body.clientHeight)});
+  await page.setViewport(override);
+  await page.setContent(content)
+  // const img = `${name}.${format}`
+  // console.log(name, format);
+  await page.screenshot({path: '顶顶顶顶.jpg'})
   await browser.close();
 })();
