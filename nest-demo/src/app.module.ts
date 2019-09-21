5fb67518a3d90bfeb3b1928dbc9b5e11a39b1a9b
Go7hic <gtfx0209@qq.com>
2019-09-21 23:52:46 +0800

    update

---
+++ nest-demo/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      definitions: {
+        path: join(process.cwd(), '/src/graphql.schema.d.ts'),
+        outputAs: 'class',
+      },
+      typePaths: ['./**/*.graphql'],
+      resolverValidationOptions: {
+        requireResolversForResolveType: false,
+      },
+    }),
+  ],
+})
+export class AppModule {}
